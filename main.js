cd283cfe194819acc3aa544b8d8d56068e0a2ef1
Hajime Miyauchi <hajime.miyauchi@gmail.com>
2020-05-30 15:26:15 +0900

    CSVの読み込みに対応

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+'use strict';
 
+function readCsv(data) {
+  var target = '#csv-body';
+  var csv = $.csv.toArrays(data);
+  var insert = '';
+  $(csv).each(function() {
+    if (this.length > 0) {
+    insert += '<tr>';
+    $(this).each(function() {
+      insert += '<td>' + this + '</td>';
+    });
+    insert += '</tr>';
+    }
+  });
+  $(target).append(insert);
+}
+
+var csvfile = 'nagoya-opendata.csv';
+$(function(){
+  $.get(csvfile, readCsv, 'text');
+});
